caa6809a010ee88a2b1bf191d8b00be2e1c1fb4e
Coty Condry <ccondry@cisco.com>
2020-09-28 02:35:39 -0500

    fix user and message lookup, and log it whether it is from a room or not

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -8,18 +8,15 @@ router.post('/*', async (req, res, next) => {
   try {
     // new messages
     if (req.body.resource === 'messages' && req.body.event === 'created') {
+      // find the related user
+      const user = await oauth2.getUser({appId: req.body.appId})
+      // and get the actual message content
+      const message = await webex(user.token.access_token).messages.get(req.body.data.id)
+      console.log('retrieved message for', user.personEmail, ':', message)
       if (req.body.data.roomType === 'group') {
         // room message
-        // find the related user
-        const user = await oauth2.getUser({appId: req.body.appId, roomId: req.body.data.roomId})
-        // and get their access token
-        const accessToken = user.token
-        // and get the actual message content
-        const message = await webex(accessToken.access_token).messages.get(req.body.data.id)
-        console.log('retrieved message', message)
       } else {
         // direct message
-        // ignore for now
       }
     }
     return res.status(200).send()
