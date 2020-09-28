ee01209ee051719ae1087f7d6ccc455d8dc5db4c
Coty Condry <ccondry@cisco.com>
2020-09-28 03:21:52 -0500

    re-enable removing @mentions from html

--- src/models/handlers/user-message.js
+++ src/models/handlers/user-message.js
@@ -5,9 +5,10 @@ const file = require('../file')
 
 module.exports = async function (user, event, targetRoomId) {
   // remove @mention html tags
-  // const mentionRegex = /<spark-mention.*<\/spark-mention>/g
-  // const html = event.data.html.replace(mentionRegex, '').trim()
-  const html = event.data.html
+  const mentionRegex = /<spark-mention.*<\/spark-mention>/g
+  const html = event.data.html.replace(mentionRegex, '').trim()
+  // const html = event.data.html
+  
   // remove @mention user name from text
   // const botName = user.displayName
   // const text = event.data.text.replace(botName, '').trim()
