b1b308eaf0b45ccf08f73a1a3746419761306ca2
Coty Condry <ccondry@cisco.com>
2020-09-28 00:06:46 -0500

    move oauth token code to models/oauth2

--- src/models/oauth2.js
+++ src/models/oauth2.js
@@ -16,8 +16,61 @@ const urlEncode = function (params) {
   return ret
 }
 
+const cache = {}
+
+// wait time in milliseconds between checking all tokens
+const throttle = 20 * 1000
+
+setInterval(async function () {
+  const keys = Object.keys(cache)
+  for (const key of keys) {
+    const value = cache[key]
+    // expiring soon?
+    const now = new Date()
+    const nowSeconds = Math.round(now.getTime() / 1000)
+    // number of seconds before refresh expires when we should perform refresh
+    const refreshBefore = Math.round(value.expires_in / 14)
+    // time after which refresh should be done
+    const refreshTime = value.created + value.expires_in - refreshBefore
+    // refresh?
+    if (nowSeconds > refreshTime) {
+      console.log(`token ${key} needs to be refreshed. refreshing now...`)
+      // time to refresh
+      try {
+        const newToken = await model.refreshToken(value.refresh_token)
+        console.log(`token ${key} refreshed successfully:`, newToken)
+        newToken.created = Math.round(now.getTime() / 1000)
+        // update cache with new token details
+        cache[key] = newToken
+      } catch (e) {
+        console.log(`token ${key} failed to refresh:`, e.message)
+      }
+    } else {
+      // how many seconds before we need to refresh
+      const t = refreshTime - nowSeconds
+      // minutes before we need to refresh
+      const m = Math.round(t / 60)
+      if (m > 60) {
+        const h = Math.round(m / 60)
+        if (h > 24) {
+          const d = Math.round(h / 24)
+          const hr = h % 24
+          const mr = m % 60
+          console.log(`token ${key} does not need to be refreshed yet. It will in ${d} days, ${hr} hours, and ${mr} minutes`)
+        } else {
+          const r = m % 60
+          console.log(`token ${key} does not need to be refreshed yet. It will in ${h} hours and ${r} minutes`)
+        }
+      } else {
+        console.log(`token ${key} does not need to be refreshed yet. It will in ${m} minutes`)
+      }
+    }
+  }
+}, throttle)
+
 module.exports = {
   getAccessToken ({code, redirectUri}) {
+    // build body object
     const body = {
       grant_type: 'authorization_code',
       client_id: process.env.OAUTH_CLIENT_ID,
@@ -25,8 +78,10 @@ module.exports = {
       code,
       redirect_uri: redirectUri
     }
+    // encode the body for x-www-form-urlencoded
     const encodedBody = urlEncode(body)
-    return fetch('https://webexapis.com/v1/access_token', {
+    // get the token from webex
+    const accessToken = await fetch('https://webexapis.com/v1/access_token', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -34,6 +89,12 @@ module.exports = {
       },
       body: encodedBody
     })
+    // set created time in seconds
+    const now = new Date()
+    accessToken.created = Math.round(now.getTime() / 1000)
+    // store token in cache
+    cache[req.body.user] = accessToken
+    return
   },
   refreshToken (refreshToken) {
     const body = {
--- src/routes/oauth2.js
+++ src/routes/oauth2.js
@@ -2,71 +2,14 @@ const express = require('express')
 const router = express.Router()
 const model = require('../models/oauth2')
 
-const cache = {}
-
-// wait time in seconds between checking all tokens
-const throttle = 5 * 1000
-
-setInterval(async function () {
-  const keys = Object.keys(cache)
-  for (const key of keys) {
-    const value = cache[key]
-    // expiring soon?
-    const now = new Date()
-    const nowSeconds = Math.round(now.getTime() / 1000)
-    // number of seconds before refresh expires when we should perform refresh
-    const refreshBefore = Math.round(value.expires_in / 14)
-    // time after which refresh should be done
-    const refreshTime = value.created + value.expires_in - refreshBefore
-    // refresh?
-    if (nowSeconds > refreshTime) {
-      console.log(`token ${key} needs to be refreshed. refreshing now...`)
-      // time to refresh
-      try {
-        const newToken = await model.refreshToken(value.refresh_token)
-        console.log(`token ${key} refreshed successfully:`, newToken)
-        newToken.created = Math.round(now.getTime() / 1000)
-        // update cache with new token details
-        cache[key] = newToken
-      } catch (e) {
-        console.log(`token ${key} failed to refresh:`, e.message)
-      }
-    } else {
-      // how many seconds before we need to refresh
-      const t = refreshTime - nowSeconds
-      // minutes before we need to refresh
-      const m = Math.round(t / 60)
-      if (m > 60) {
-        const h = Math.round(m / 60)
-        if (h > 24) {
-          const d = Math.round(h / 24)
-          const hr = h % 24
-          const mr = m % 60
-          console.log(`token ${key} does not need to be refreshed yet. It will in ${d} days, ${hr} hours, and ${mr} minutes`)
-        } else {
-          const r = m % 60
-          console.log(`token ${key} does not need to be refreshed yet. It will in ${h} hours and ${r} minutes`)
-        }
-      } else {
-        console.log(`token ${key} does not need to be refreshed yet. It will in ${m} minutes`)
-      }
-    }
-  }
-}, throttle)
-
 // save client access code
 router.post('/', async (req, res, next) => {
   try {
-    // console.log('req.headers.host =', req.headers.host)
-    console.log('req =', req)
-    const accessToken = await model.getAccessToken({
+    // get access token from webex
+    await model.getAccessToken({
       code: req.body.code,
-      redirectUri: 'https://mm-helper.cxdemo.net/oauth2/'
+      redirectUri: req.headers.referer.split('?')[0]
     })
-    // set created time in seconds
-    const now = new Date()
-    accessToken.created = Math.round(now.getTime() / 1000)
-    cache[req.body.user] = accessToken
     return res.status(200).send()
   } catch (e) {
     console.log(`Failed to save oauth2 access token:`, e.message)
